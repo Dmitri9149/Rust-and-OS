8b51e29a35391b86814bcda06e0b1193e19f99f1
Dmitri9149 <dmitri.as.name@gmail.com>
2024-01-20 17:22:13 +0200

    add comments

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 // 'panic abort' are added to Cargo to eliminate the eh_personality 
 // errors, it stops unwinding of stack after abort 
 
+// has to implement our own entry point (not to use crt0 of C)
+
 use core::panic::PanicInfo;
 // the function which is called on panic
 #[panic_handler]
