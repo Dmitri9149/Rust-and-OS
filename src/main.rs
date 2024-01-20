05627c055cd36fe7e692f9591485849559dd792e
Dmitri9149 <dmitri.as.name@gmail.com>
2024-01-20 15:53:38 +0200

    attempt to implement own panic function

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,9 @@
 #![no_std] // no std library
+
+use core::panic::PanicInfo;
+// the function which is called on panic
+#[panic_handler]
+fn panic(_info:&PanicInfo) -> ! {
+    loop {}
+}
 fn main() {} // eliminate println! , not working without std::
