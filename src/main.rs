ac3fc9a032add37e84e3ffd90682dc7f4cf3ad08
Dmitri9149 <dmitri.as.name@gmail.com>
2024-01-20 18:01:24 +0200

    add comments

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
-#![no_std] // no std library
-#![no_main] // will not use normal entry point
+#![no_std] // don't link Rust  std library
+#![no_main] // disable all Rust-level entry points
 // 'panic abort' are added to Cargo to eliminate the eh_personality 
 // errors, it stops unwinding of stack after abort 
 
 // has to implement our own entry point (not to use crt0 of C)
 
-//GET LINKER ERROR AT THE STAGE, IT IS BECAUSE DEFAULT CONFIGURATION 
-// FOR LINKER RELY ON THE C LIBRARY
-// error: linking with `cc` failed: exit status: 1
-
 use core::panic::PanicInfo;
+
 // the function which is called on panic
 #[panic_handler]
 fn panic(_info:&PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle] // compile should output function with really name 'start'
+#[no_mangle] // compile should output function with name 'start', do not mangle
             // use C calling convention 'extern "C"' 
 pub extern "C" fn _start () -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
     loop {}
 }
 
