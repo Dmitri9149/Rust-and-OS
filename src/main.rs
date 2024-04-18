9efb86dc0135ee05fa74709b41f6171e3ab1cf9c
Dmitri9149 <dmitri.as.name@gmail.com>
2024-04-18 18:26:23 +0300

    fix bug in vga_buffer pointer , to be '0xb8000', not '0xb800'

--- src/main.rs
+++ src/main.rs
@@ -5,37 +5,30 @@
 
 // has to implement our own entry point (not to use crt0 of C)
 
-#![allow(unused_imports)]
 use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World"; // the VGA buffer is located at address 0xb8000
 
-// the function which is called on panic
-#[panic_handler]
-#[cfg(not(test))] // has to add this for not to have the error messages from 
-// rust analyser 
-// see the https://github.com/rust-lang/rust-analyzer/issues/4490
-// solution from 'andyduplain' 
-fn panic(_info:&PanicInfo) -> !{
-    loop {}
-}
-
-#[no_mangle] // compile should output function with name 'start', do not mangle
-            // use C calling convention 'extern "C"' 
-pub extern "C" fn _start () -> ! {
+// compile should output function with name 'start', do not mangle 
+// use C calling convention 'extern "C"'
     // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
-    
-    let vga_buffer = 0xb800 as *mut u8;
-
+    // named `_start` by default 
+#[no_mangle] 
+pub extern "C" fn _start() -> ! {    
+    let vga_buffer = 0xb8000 as *mut u8;
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) =0xb; 
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
         }
     }
+    loop {}
+}
 
-
+// the function which is called on panic
+#[panic_handler]
+ 
+fn panic(_info:&PanicInfo) -> !{
     loop {}
 }
 
