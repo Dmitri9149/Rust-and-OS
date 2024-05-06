f81319722ab16d0fdc57b5cbf28165295d57cd25
Dmitri9149 <dmitri.as.name@gmail.com>
2024-05-06 16:08:13 +0300

    print directly from _start function

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,9 @@ mod vga_buffer;
     // named `_start` by default 
 #[no_mangle] 
 pub extern "C" fn _start() -> ! {
-/*   
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-*/
-    vga_buffer::print_something();
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
 
     loop {}
 }
