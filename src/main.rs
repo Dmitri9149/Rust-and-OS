98cde53f50c5d0f35f25d340323d95daedc2e08c
Dmitri9149 <dmitri.as.name@gmail.com>
2024-05-06 16:47:34 +0300

    use println! in _start function

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,13 @@ mod vga_buffer;
 #[no_mangle] 
 pub extern "C" fn _start() -> ! {
     use core::fmt::Write;
+
+    println!(" Hello World");
+
     vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+
     write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+    
 
     loop {}
 }
